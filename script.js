56b9e95cad1b4b960fb2ebdb2c356611262ae7a5
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2025-05-19 13:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	const trimmed = s.trim();
+	if(trimmed === "") return "";
+	const spaceIndex = trimmed.indexOf(" ");
+	if(spaceIndex === -1) return trimmed;
+	return trimmed.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));  
